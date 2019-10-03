1c037f279350ff52bcbd8badc1d882a8ee6909c7
Yang Chen <yangchen@Yangs-MacBook-Pro.local>
2019-10-03 17:29:13 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardsTwo = cards[3];
 var cardsInPlay = [];
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardsTwo");
-if (cardsInPlay[0] === cardsInPlay[3]){
-	alert("You found a match!");
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry try again!");
+  console.log("Sorry, try again.");
 }
-cardsInPlay;
-console.log("User flipped queen");
-console.log("User flipped king");
+};
+function filpCard(cardId) {
+console.log("User flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+filpCard(0);
+filpCard(2);
