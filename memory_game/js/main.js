e4c35a0b5d24547e084f562642acfc43f9d3afe7
Yang Chen <yangchen@Yangs-MacBook-Pro.local>
2019-10-04 20:09:57 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "heart",
+	cardImage: "images/queen-of-hearts.png"
+}, 
+{   rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+}, 
+{
+    rank: "king",
+	suit: "heart",
+	cardImage: "images/king-of-hearts.png"
+},  
+{   rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -9,8 +28,11 @@ function checkForMatch(){
 }
 };
 function filpCard(cardId) {
-console.log("User flipped" + " " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + " " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
 checkForMatch();
 };
 filpCard(0);
