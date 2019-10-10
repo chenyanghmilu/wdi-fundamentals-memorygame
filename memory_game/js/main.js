33e7e0f23c053064c42646bfe088ac0bf6123599
Yang Chen <yangchen@Yangs-MacBook-Pro.local>
2019-10-09 21:59:28 -0400

    created gamelogic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,18 +22,29 @@ var cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 };
 function filpCard(cardId) {
+	var cardId = this.getAttribute('data-id');
 console.log("User flipped" + " " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
+if (cardsInPlay.length===2) {
 checkForMatch();
+}
 };
-filpCard(0);
-filpCard(2);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);	
+		cardElement.addEventListener('click', filpCard);
+	}
+};
+createBoard();
